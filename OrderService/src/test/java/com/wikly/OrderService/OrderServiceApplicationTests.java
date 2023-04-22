47f361f502b3f68564c3932d00fa4f45fca2d7a4
Satish Shingrakhiya <satish.shingrakhiy@gmail.com>
2023-04-22 23:40:27 +0530

    Add Order Service

---
+++ OrderService/src/test/java/com/wikly/OrderService/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wikly.OrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
